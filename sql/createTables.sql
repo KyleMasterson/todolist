9ad9b416a4d7e6c571780a3bfd635b8b3ac985f3
Pat Mollins <32046493+PatMollins@users.noreply.github.com>
2018-11-09 15:29:25 -0400

    Update createTables.sql
    Removed access table and updated users

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,16 +1,17 @@
-CREATE TABLE IF NOT EXISTS lists(
+CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
+    username VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS lists(
     id INT AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     PRIMARY KEY(id)
 );
 
@@ -19,20 +20,7 @@ CREATE TABLE IF NOT EXISTS items(
     list_id INT,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    FOREIGN KEY (id) REFERENCES lists(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS access(
-    id INT AUTO_INCREMENT,
-    list_id INT,
-    user_id INT,
     FOREIGN KEY (list_id) REFERENCES lists(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     PRIMARY KEY(id)
