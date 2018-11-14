f323a7098157f56994ea037084074cd8413029a7
Pat Mollins <32046493+PatMollins@users.noreply.github.com>
2018-11-14 16:57:37 -0400

    Combined Create User and Login Procedures

--- sql/storedProcedure.sql
+++ sql/storedProcedure.sql
@@ -1,7 +1,6 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS login //
-DROP PROCEDURE IF EXISTS createUser //
 DROP PROCEDURE IF EXISTS getLists //
 DROP PROCEDURE IF EXISTS createList //
 DROP PROCEDURE IF EXISTS getListById //
@@ -13,29 +12,27 @@ DROP PROCEDURE IF EXISTS deleteItem //
 
 CREATE PROCEDURE login(IN name VARCHAR(255))
 BEGIN
- SELECT id FROM users
-  WHERE username = name;
+IF EXISTS (SELECT username FROM users WHERE username = name) THEN
+  SELECT username FROM users WHERE username = name;
+ ELSE
+  INSERT INTO users(username) VALUES (name);
+ END IF;
 END //
-   
-CREATE PROCEDURE createUser(IN name VARCHAR(255))
-BEGIN
- INSERT INTO users(username) VALUES (name);
-END//
   	
-CREATE PROCEDURE getLists(IN userID int(11))
+CREATE PROCEDURE getLists(IN userID VARCHAR(255))
 BEGIN
  SELECT * FROM lists
-  where user_id = userID;
+  where user_name = userID;
 END//
 	
-CREATE PROCEDURE createList(IN userID INT(11), inTitle VARCHAR(255), inDescription text)
+CREATE PROCEDURE createList(IN userID VARCHAR(255), inTitle VARCHAR(255), inDescription text)
 BEGIN
- INSERT INTO lists(user_id, title, description) VALUES (userID, inTitle, inDescription);
+ INSERT INTO lists(user_name, title, description) VALUES (userID, inTitle, inDescription);
 END//
 	
 CREATE PROCEDURE getListById(IN listID INT(11))
 BEGIN
-  SELECT * FROM lists
+ SELECT * FROM lists
   WHERE id = listID;
 END//
 	
