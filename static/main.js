4f2b9061cfcff7d8d7e1b08b25e8f94702363fd4
Kyle Masterson <Kyle.Masterson@unb.ca>
2018-11-24 17:11:10 -0400

    Login page basics

--- static/main.js
+++ static/main.js
@@ -1,66 +1,29 @@
-var app=new Vue({
-  el: '#app',
+var user = {
+  username:'',
+  password: ''
+};
+
+var app = new Vue({
+  el: '#vueRoot',
   data: {
-    // status: '',
-    schs:  '',
-    sch: '',
-    addedSch: ''
-  },
-  created: function () {
+    user: user,
+    res: ''
   },
   methods:{
-      addSchool : function (){
-
-        var postData = {
-          Name: "sch2",
-          Province: "NB",
-          Language: "EN",
-          Level: "simple"
-
-            };
-          let axiosConfig = {
-              headers: {
-                  'Content-Type': 'application/json;charset=UTF-8',
-                  "Access-Control-Allow-Origin": "*",
-              }
-            };
-            axios.post('http://info3103.cs.unb.ca:61011/schools', postData, axiosConfig)
-            .then((res) => {
-              app.addedSch=res.data;
-            })
-            .catch((err) => {
-              app.addedSch= err;
-            });
-    },//end addSchool
-
-        schoolIDget: function (schid) {
-          // this.status='loading';
-          var app=this;
-          axios.get('http://info3103.cs.unb.ca:61011/schools/'+schid)
-          .then(function (response){
-            app.sch=response.data;
-          })
-          .catch(function(error){
-            app.sch='An error ocurred: '+error;
-
-          });
-
-        },//end schoolIDget
-
-        schoolsget: function () {
-          // this.status='loading';
-          var app=this;
-          axios.get('http://info3103.cs.unb.ca:61011/schools')
-          .then(function (response){
-            app.schs=response.data.schools;
-          })
-          .catch(function(error){
-            app.schs='An error ocurred: '+error;
-
-          });
-
-        },//end schoolsget
-
-      }//end methods
-
-    })//end Vue
+    signIn : function (){
+      let axiosConfig = {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+        }
+      };
+      axios.post('http://info3103.cs.unb.ca:24842/signin', user, axiosConfig)
+      .then((res) => {
+        app.res=res.data;
+      })
+      .catch((err) => {
+        app.res= err;
+      });
+    },
+  }
+})
