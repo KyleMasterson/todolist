82c39c1dc7fa8a640794219e575bd7d1ec3706c6
Kyle Masterson <Kyle.Masterson@unb.ca>
2018-12-03 20:36:20 -0400

    Added back button to error page

--- front-end/src/components/Error.vue
+++ front-end/src/components/Error.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
         <h1>Looks like you wandered off the beaten path</h1>
+        <button type="button" class="btn btn-primary" v-on:click="goToIndex">Back to Safety</button>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        goToIndex: function(){
+            this.$router.push('/');
+        }
+    }
+}
+</script>
